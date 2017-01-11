981e4a39dceab959a95217795260e7ab537244a8
Vlad Romanenko <denko1024@gmail.com>
2017-01-11 16:26:44 +0200

    Add 2 sliders and smooth scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,19 +8,52 @@
 		$('#banner-slider').slick({
 			slide: '.ba-slide',
 			prevArrow: '#banner-slider .ba-slider__arrow--left',
-			nextArrow: '#banner-slider .ba-slider__arrow--right'
+			nextArrow: '#banner-slider .ba-slider__arrow--right',
+			dots: true
 		});
 
 		updateCurrentSlide();
+		
 		$('#banner-slider').on('afterChange', function(e) {
 			updateCurrentSlide();
 		});
 
+		$('#banner-slider .slick-dots li').each(function() {
+			$(this).find('button').text( '0' + $(this).find('button').text() );
+		});
+
+
+		// shop sliders
+		$('#ba-shop-big .ba-slider').slick({
+			slide: '.ba-slide',
+			arrows: false,
+			asNavFor: '#ba-shop .ba-slider',
+		});
+		$('#ba-shop .ba-slider').slick({
+			slide: '.ba-slide',
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			asNavFor: '#ba-shop-big .ba-slider',
+			centerMode: true 
+		});	
+
+		// smooth scroll
+		$('a[href*="#"]').on('click', function(event) {
+			event.preventDefault();
+			var where = $(this).attr('href');
+			$('body, html').animate({
+				scrollTop: $(where).offset().top, 
+			}, 600); 
+		});
 	});
+
+
 
 	function updateCurrentSlide() {
 		var currentSlideIndex = $('#banner-slider .slick-active').data('slick-index') + 1;
 		$('#banner-slider .ba-current-position').text(currentSlideIndex);
 	}
+
+
 
 })(jQuery);
